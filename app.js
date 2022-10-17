ea384f5f8b65bb10d771bb3f45e32027fc64b67c
Alex <627519@bah.com>
2022-10-17 14:35:24 -0400

    updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Just finished building and deploying my first AWS Pipeline'));
+app.get('/', (req, res) => res.send('Demo run October 10'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

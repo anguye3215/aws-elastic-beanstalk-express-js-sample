591fc65d61bcb495c16226a4e19d7b4af324ce74
Alex <627519@bah.com>
2022-10-17 15:13:51 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Demo run October 10'));
+app.get('/', (req, res) => res.send('Updated change 3:13pm'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
